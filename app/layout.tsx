64ce623727355cc0d8b642eb21a495b388bc4e6e
Amirul Syahril <amirsyah.1989@gmail.com>
2026-05-01 19:27:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'My Google AI Studio App',
-  description: 'My Google AI Studio App',
+  title: 'Simulasi Tapir Hero | My Digital Competency Score',
+  description: 'Simulasi Tapir Hero',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
